9ad3c4c62fce2f79cbe3f72be4e5c30a8fb5ccd8
sunliqian <liqian.sun@ucextech.com>
2019-12-20 11:14:44 +0800

    增加日志

--- springbootexample/multidatasource/src/test/java/com/xiayu/user/BusinessTest.java
+++ springbootexample/multidatasource/src/test/java/com/xiayu/user/BusinessTest.java
@@ -4,6 +4,8 @@ import com.xiayu.entity.BusinessEntity;
 import com.xiayu.mapper.BusinessMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,12 +16,23 @@ public class BusinessTest {
     @Autowired
     private BusinessMapper businessMapper;
 
+    Logger log = LoggerFactory.getLogger(BusinessTest.class);
+
     @Test
     public void testInsertOrder(){
         BusinessEntity businessEntity = new BusinessEntity();
-        businessEntity.setId(2);
+        businessEntity.setId(4);
         businessEntity.setBusinessDetail("businessdetail");
         businessMapper.insertBusiness(businessEntity);
+        log.info("success");
+        log.error("failed");
     }
 
+    @Test
+    public void testLog(){
+        log.error("error");
+        log.info("info");
+        log.debug("debug");
+        log.trace("trace");
+    }
 }
